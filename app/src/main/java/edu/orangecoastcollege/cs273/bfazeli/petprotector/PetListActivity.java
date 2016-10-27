9c4f9c50f34a38f4f217c101e95f2859936ffef6
bfazeli <bfazeli1@gmail.com>
2016-10-27 15:14:19 -0700

    Implemented getUriToResource and completed application.

--- app/src/main/java/edu/orangecoastcollege/cs273/bfazeli/petprotector/PetListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/bfazeli/petprotector/PetListActivity.java
@@ -1,22 +1,24 @@
 package edu.orangecoastcollege.cs273.bfazeli.petprotector;
 
 import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.AnyRes;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class PetListActivity extends AppCompatActivity {
 
@@ -25,6 +27,7 @@ public class PetListActivity extends AppCompatActivity {
     // This member variable stores the URI to whatever image has been selected
     // Default: none.png (R.drawable.none)
     private Uri imageUri;
+    private static final int REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,15 +63,13 @@ public class PetListActivity extends AppCompatActivity {
         if (writeExternalStoragePermission != PackageManager.PERMISSION_GRANTED)
             permList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
-        int requestCode = 100;
-
         // If the list has items (size > 0), we need to request permissions from the user:
         if (permList.size() > 0)
         {
             // Convert the ArrayList into an Array of Strings
             String[] perms = new String[permList.size()];
             // Request permissions from the user
-            ActivityCompat.requestPermissions(this, permList.toArray(perms), requestCode);
+            ActivityCompat.requestPermissions(this, permList.toArray(perms), REQUEST_CODE);
         }
 
         // If we have all 3 permissions, open ImageGallery:
@@ -78,11 +79,46 @@ public class PetListActivity extends AppCompatActivity {
         {
             // Use an Intent to launch gallery and take pictures
             Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(galleryIntent, requestCode);
+            startActivityForResult(galleryIntent, REQUEST_CODE);
         } else Toast.makeText(this,
                 "Per protector requires camera and external storage permission",
                 Toast.LENGTH_LONG).show();
     }
 
-    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Code to handle when the user closes the image gallery
+        // (by selecting an image or pressing the back button).
+
+        // The intent data is the URI selected from image gallery
+        // Decide if the user selected an image:
+        if (data != null && requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            // Set the imageURI to the data
+            imageUri = data.getData();
+            petImageView.setImageURI(imageUri);
+        }
+    }
+
+    /**
+     * Get uri to any resource type within an Android Studio project. Method is public static
+     * to allow other classes to use it as a helper function.
+     *
+     * @param context The current context
+     * @param resId   The resource identifier of the drawable
+     * @return  Uri to resource by given id
+     * @throws Resources.NotFoundException If the given resource id does not exist.
+     */
+    public static Uri getUriToResource(@NonNull Context context, @AnyRes int resId) throws Resources.NotFoundException {
+        /**Return a Resources instance for your application's package. */
+        Resources res = context.getResources();
+        /** return URI */
+        return Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE +
+                "://" + res.getResourcePackageName(resId)
+                + '/' + res.getResourceTypeName(resId)
+                + '/' + res.getResourceEntryName(resId));
+    }
+
+
 }
